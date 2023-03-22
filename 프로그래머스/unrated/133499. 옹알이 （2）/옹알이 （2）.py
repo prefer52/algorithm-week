d740c451ef3f62bb109e91feae30b30552f43264
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-03-22 17:57:16 +0900

    [unrated] Title: 옹알이 (2), Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,19 +1,17 @@
 def solution(babbling):
+    word = [['a','w'],['aya','woo'],['y','m'],['ye','ma']]
     result = 0
     for i in babbling:
-        j = 0
-        pre_word = ''
+        j, pre_word, next = 0, '', 0
         while j < len(i):
-            if (i[j] == 'a' or i[j] == 'w') and (j+2 < len(i)):
-                if (i[j:j+3] == 'aya' or i[j:j+3] == 'woo') and (i[j:j+3] != pre_word): j += 3
+            if (i[j] in word[0]) and (j+2 < len(i)):
+                if (i[j:j+3] in word[1]) and (i[j:j+3] != pre_word): next = 3
                 else: break
-                pre_word = i[j-3:j]
-            elif (i[j] == 'y' or i[j] == 'm') and (j+1 < len(i)):
-                if (i[j:j+2] == 'ye' or i[j:j+2] == 'ma') and (i[j:j+2] != pre_word): j+= 2
+            elif (i[j] in word[2]) and (j+1 < len(i)):
+                if (i[j:j+2] in word[3]) and (i[j:j+2] != pre_word): next = 2
                 else: break
-                pre_word = i[j-2:j]
-            else:
-                break
+            else: break
+            pre_word = i[j:j+next]
+            j += next
         if j >= len(i): result += 1
-    
     return result
