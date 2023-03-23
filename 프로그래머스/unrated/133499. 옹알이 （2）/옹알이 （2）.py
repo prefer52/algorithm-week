9a5f676f44bc2e26ba145bc1ae95ad56b9dc2b4b
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-03-23 22:18:48 +0900

    [unrated] Title: 옹알이 (2), Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,16 +1,6 @@
 def solution(babbling):
     result = 0
     for i in babbling:
-        j, pre_word, next = 0, '', 0
-        while j < len(i):
-            if (i[j] in ['a', 'w']) and (j+2 < len(i)):
-                if (i[j:j+3] in ['aya', 'woo']) and (i[j:j+3] != pre_word): next = 3
-                else: break
-            elif (i[j] in ['y', 'm']) and (j+1 < len(i)):
-                if (i[j:j+2] in ['ye', 'ma']) and (i[j:j+2] != pre_word): next = 2
-                else: break
-            else: break
-            pre_word = i[j:j+next]
-            j += next
-        if j >= len(i): result += 1
+        if '*' in i.replace('ayaaya', '*').replace('woowoo', '*').replace('mama', '*').replace('yeye', '*'): continue
+        if i.replace('aya', ' ').replace('woo', ' ').replace('ma', ' ').replace('ye', ' ').replace(' ', '') == '': result += 1
     return result
