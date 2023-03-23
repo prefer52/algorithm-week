9dca589909d4cc46fa003e9f3865a821eb4a5b4d
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-03-23 22:25:50 +0900

    Update 옹알이 （2）.py

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -2,5 +2,5 @@ def solution(babbling):
     result = 0
     for i in babbling:
         if '*' in i.replace('ayaaya', '*').replace('woowoo', '*').replace('mama', '*').replace('yeye', '*'): continue
-        if i.replace('aya', ' ').replace('woo', ' ').replace('ma', ' ').replace('ye', ' ').replace(' ', '') == '': result += 1
+        if i.replace('aya', ' ').replace('woo', ' ').replace('ma', ' ').replace('ye', ' ').strip() == '': result += 1
     return result
