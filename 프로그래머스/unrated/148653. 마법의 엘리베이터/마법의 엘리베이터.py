cd8d29a73c7cd97486e37629230df6956802337b
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-03-24 20:04:40 +0900

    Update 마법의 엘리베이터.py

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,7 +1,5 @@
 def solution(storey):
     fm, result = [0,1,2,3,4,5,4,3,2,1], 0
     while storey // 1 > 0:
-        result += fm[storey%10]
-        if storey%10 < 5 or (storey%10 == 5 and storey//10%10 < 5): storey //= 10
-        else: storey = storey//10 + 1
+        result, storey =  result + fm[storey%10], storey // 10 if storey%10 < 5 or (storey%10 == 5 and storey//10%10 < 5) else storey//10 + 1
     return result
