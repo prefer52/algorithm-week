15bf9b1e92ebd7deab9187aca8203d84288d1b10
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-03-29 15:04:47 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.29 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -2,18 +2,14 @@ def solution(today, terms, privacies):
     result = []
     year, month, day = map(int, today.split('.'))
     day_sum = year*10000+(month-1)*100+day
-    
     clause = {i.split()[0]:int(i.split()[1]) for i in terms}
     
     for i, info in enumerate(privacies):
-        y, m, d = map(int, info.split()[0].split('.'))
-        cla = str(info.split()[1])
-        days = y*12*28 + (m-1)*28 + d + (clause[cla]-1)*28 + 28
+        date, cla = info.split()
+        y, m, d = map(int, str(date).split('.'))
+        days = y*12*28 + (m-1)*28 + d + clause[cla]*28
         y, m = divmod(days, (12*28))
         m, d = divmod(m , 28)
-        if day_sum >= y*10000+m*100+d:
-            result.append(i+1)
+        if day_sum >= y*10000+m*100+d: result.append(i+1)
             
     return result
-
-print(solution('2022.05.19', ['A 6', 'B 12', 'C 3'], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))
