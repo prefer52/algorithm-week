817c59485fdb98de39f9c2f90ba5754d6fdf0313
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-04-05 22:30:16 +0900

    Update 햄버거 만들기.py

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,9 +1,7 @@
 def solution(ingredient):
     hamburger = ''.join(list(map(str, ingredient)))
-    burger, result = ['0', '0', '0', '0'], 0
+    burger = ['0', '0', '0']
     for i in hamburger:
         burger.append(i)
-        if burger[-4:] == ['1', '2', '3', '1']:
-            del(burger[-4:])
-            result += 1
-    return result
+        if burger[-4:] == ['1', '2', '3', '1']: del(burger[-4:])
+    return (len(ingredient) - (len(burger)-3))//4
