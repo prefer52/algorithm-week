3e492d50a880470c06650f255fdd2b04d4a50250
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-04-08 02:38:40 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -9,14 +9,11 @@ def solution(places):
         i = wall + [['O']*2 + list(j) + ['O']*2 for j in i] + wall #out of range 방지
         for x, y in product(range(2,7), repeat=2):
             if i[x][y] == 'P':
-                if any([i[x+a][y+b] == 'P' for a, b in plus]):
-                    result[idx] = 0
-                    break
-                if any([i[x+2*a][y+2*b] + i[x+a][y+b] == 'PO' for a, b in plus]):
-                    result[idx] = 0
-                    break
+                violate = any([i[x+a][y+b] == 'P' for a, b in plus])
+                violate = violate or any([i[x+2*a][y+2*b] + i[x+a][y+b] == 'PO' for a, b in plus])
                 x_str = ''.join([i[x+a][y+b] for a, b in X])
-                if 'PO' in x_str or 'OP' in x_str:
+                violate = violate or ('PO' in x_str or 'OP' in x_str)
+                if violate:
                     result[idx] = 0
                     break
     return result
