a1aaf09c68dbbd2766ffbedf447e7997d80b80ad
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-04-08 02:56:22 +0900

    Update 거리두기 확인하기.py

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,7 +1,7 @@
 from itertools import product
 
 def solution(places):
-    result, plus = [1, 1, 1, 1, 1], [(-1,0), (0, -1), (1, 0), (0, 1)]
+    result, plus = [0, 0, 0, 0, 0], [(-1,0), (0, -1), (1, 0), (0, 1)]
     X = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
     
     for idx, i in enumerate(places):
@@ -9,11 +9,9 @@ def solution(places):
         i = wall + [['O']*2 + list(j) + ['O']*2 for j in i] + wall #out of range 방지
         for x, y in product(range(2,7), repeat=2):
             if i[x][y] == 'P':
-                violate = any([i[x+a][y+b] == 'P' for a, b in plus])
-                violate = violate or any([i[x+2*a][y+2*b] + i[x+a][y+b] == 'PO' for a, b in plus])
+                if any([i[x+a][y+b] == 'P' for a, b in plus]): break
+                if any([i[x+2*a][y+2*b] + i[x+a][y+b] == 'PO' for a, b in plus]): break
                 x_str = ''.join([i[x+a][y+b] for a, b in X])
-                violate = violate or ('PO' in x_str or 'OP' in x_str)
-                if violate:
-                    result[idx] = 0
-                    break
+                if 'PO' in x_str or 'OP' in x_str: break
+        else: result[idx] = 1
     return result
