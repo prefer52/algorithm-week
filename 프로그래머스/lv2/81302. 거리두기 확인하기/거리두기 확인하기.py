5c1a2a8e108e4746aad3fda132be094523a6680d
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-04-07 12:19:55 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,22 @@
+from itertools import product
+
+def solution(places):
+    result, plus = [1, 1, 1, 1, 1], [(-1,0), (0, -1), (1, 0), (0, 1)]
+    X = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
+    
+    for idx, i in enumerate(places):
+        wall = [list('O'*9)]*2
+        i = wall + [['O']*2 + list(j) + ['O']*2 for j in i] + wall #out of range 방지
+        for x, y in product(range(2,7), repeat=2):
+            if i[x][y] == 'P':
+                if any([i[x+a][y+b] == 'P' for a, b in plus]):
+                    result[idx] = 0
+                    break
+                if any([i[x+2*a][y+2*b] + i[x+a][y+b] == 'PO' for a, b in plus]):
+                    result[idx] = 0
+                    break
+                x_str = ''.join([i[x+a][y+b] for a, b in X])
+                if 'PO' in x_str or 'OP' in x_str:
+                    result[idx] = 0
+                    break
+    return result
