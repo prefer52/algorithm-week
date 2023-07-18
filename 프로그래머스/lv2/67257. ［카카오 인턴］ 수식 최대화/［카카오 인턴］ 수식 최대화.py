9e69807cbcf648023b2ba18130c910b5e9b23add
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-07-18 12:56:21 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.79 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -7,7 +7,7 @@ def solution(expression):
     
     for i in op:
         num_only = num_only.replace(i, ' ')
-    num_only = list(map(int, num_only.split()))
+    num_only = num_only.split()
     for i in expression:
         if i in op:
             op_only += i
@@ -18,14 +18,9 @@ def solution(expression):
         for i in case:
             while op_list.find(i) != -1:
                 index = op_list.find(i)
-                if i == '+':
-                    num_list[index] += num_list[index+1]
-                elif i == '-':
-                    num_list[index] -= num_list[index+1]
-                else:
-                    num_list[index] *= num_list[index+1]
+                num_list[index] = eval(str(num_list[index]) + i + str(num_list[index+1]))
                 del num_list[index+1]
                 op_list = op_list.replace(i, '', 1)
-        if abs(num_list[0]) > max:
-            max = abs(num_list[0])
+        if abs(int(num_list[0])) > max:
+            max = abs(int(num_list[0]))
     return max
