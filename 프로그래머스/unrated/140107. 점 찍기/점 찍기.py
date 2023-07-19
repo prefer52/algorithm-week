a458167794a36e855c7e0f0c75ec787125385015
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-07-19 15:49:02 +0900

    [unrated] Title: 점 찍기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
@@ -1,4 +1,5 @@
 def solution(k, d):
-    max_x = k * (d//k)
-    result = [int((d*d-x*x)**0.5)//k for x in range(0, max_x+1, k)]
-    return sum(result) + (max_x//k + 1)
+    max_x, result = k * (d//k), 0
+    for x in range(0, max_x+1, k):
+        result += int((d*d-x*x)**0.5)//k+1
+    return result
