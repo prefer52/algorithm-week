51aaf85548dbc38f95303841eec9a10ec4412195
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-08-05 23:17:39 +0900

    [unrated] Title: 롤케이크 자르기, Time: 439.27 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,15 +1,18 @@
 def solution(topping):
     result = 0
-    topping_set = set()
+    topping_type = set()
     left_bread, right_bread = [], []
+    
     for i in topping:
-        topping_set.add(i)
-        left_bread.append(len(topping_set))
-    topping_set.clear()
+        topping_type.add(i)
+        left_bread.append(len(topping_type))
+    topping_type.clear()
+    
     for i in list(reversed(topping)):
-        topping_set.add(i)
-        right_bread.append(len(topping_set))
+        topping_type.add(i)
+        right_bread.append(len(topping_type))
     right_bread.reverse()
+    
     for i in range(0, len(topping)-1):
         if left_bread[i] == right_bread[i+1]:
             result += 1
