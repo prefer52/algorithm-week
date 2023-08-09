8505fe9a65542a830c30b1c0c35cc60b743bb9e6
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-08-10 00:00:44 +0900

    [level 2] Title: 방문 길이, Time: 0.64 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,18 @@
+def solution(dirs):
+    pos = [0, 0]
+    move = {'U':(0,1), 'D':(0,-1), 'R':(1,0), 'L':(-1,0)}
+    path_set = set()
+    
+    for dir in dirs:
+        cur_pos = pos[:]
+        for i in range(2):
+            if abs(pos[i] + move[dir][i]) <= 5:
+                pos[i] += move[dir][i]
+        if cur_pos == pos:
+            continue
+        path_set.add(tuple(cur_pos + pos))
+        path_set.add(tuple(pos + cur_pos))
+
+    return len(path_set)//2
+
+solution("LULLLLLLU")
