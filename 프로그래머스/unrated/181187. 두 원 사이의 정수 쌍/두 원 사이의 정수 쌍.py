8404e8c45472231f1cd36f830b4ec3d28b1c0f82
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-08-13 17:38:47 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 715.69 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,20 @@
+import math
+
+def solution(r1, r2):
+    r1_circle, r2_circle = 0, 0
+    
+    for x in range(1, r2):
+        r2_circle += int((r2**2 - x**2)**0.5)
+    r2_circle = r2_circle*4 + r2*4 + 1
+    
+    for x in range(1, r1):
+        y = ((r1**2 - x**2)**0.5)
+        if math.ceil(y-1) == int(y):
+            r1_circle += int(y)
+        else:
+            r1_circle += int(y-1)
+    r1_circle = r1_circle*4 + (r1-1)*4 + 1
+    
+    print(r1_circle, r2_circle)
+    
+    return r2_circle - r1_circle
