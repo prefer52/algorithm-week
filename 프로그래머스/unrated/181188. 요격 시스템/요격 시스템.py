5cd4a414bdf93cc800b9aed0002478e110498c5b
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-08-20 02:28:30 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,11 +1,11 @@
 def solution(targets):
     targets.sort()
-    answer, min_missile = 0, targets[0]
+    answer, min_e = 0, targets[0][1]
     for target in targets:
-        if min_missile[0] <= target[0] < min_missile[1]:
-            min_missile = [target[0], min(target[1], min_missile[1])]
+        if target[0] < min_e:
+            min_e = min(target[1], min_e)
         else:
-            min_missile = target
+            min_e = target[1]
             answer += 1
-                        
+            
     return answer+1
