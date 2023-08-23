c8bd07ca1f739a5e189ee1099c8c7929e7513d60
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-08-23 16:42:03 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,10 +1,6 @@
 def solution(n):
     battery_use = 0
     while n > 0:
-        if n%2 == 0:
-            n //= 2
-        else:
-            n -= 1
-            battery_use += 1
-    
+        battery_use += n%2
+        n = n>>1 if n%2 == 0 else n-1
     return battery_use
