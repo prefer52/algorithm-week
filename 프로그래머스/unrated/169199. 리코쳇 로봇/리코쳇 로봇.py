f3b3a98932f0df6e07a1a1591309301a5e214f00
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-08-31 17:44:12 +0900

    [unrated] Title: 리코쳇 로봇, Time: 0.55 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -1,3 +1,30 @@
+from collections import deque
+
 def solution(board):
-    answer = 0
-    return answer
+    board = ['D' + row + 'D' for row in board]
+    board = ['D'*len(board[0])] + board + ['D'*len(board[0])]
+    visited = [[False] * len(board[0]) for _ in range(len(board))]
+    moves = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+
+    R_pos = tuple()
+    for y, row in enumerate(board):
+        if 'R' in row:
+            R_pos = (row.find('R'), y, 0)
+    
+    queue = deque([R_pos])
+    while queue:
+        pos = queue.popleft()
+        if board[pos[1]][pos[0]] == 'G':
+            return pos[2]
+        
+        visited[pos[1]][pos[0]] = True
+        for move in moves:
+            x, y, moved = pos
+            while board[y][x] != 'D':
+                x += move[0]
+                y += move[1]
+            
+            if not visited[y-move[1]][x-move[0]]:
+                visited[y-move[1]][x-move[0]] = True
+                queue.append((x-move[0], y-move[1], moved+1))
+    return -1
