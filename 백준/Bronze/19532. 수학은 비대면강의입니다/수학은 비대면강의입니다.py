533d9d3b627003ccaae8f7a7a370e11916ce3d7d
prefer52 <110543937+prefer52@users.noreply.github.com>
2023-09-10 23:30:42 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 684 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,8 @@
+from sys import stdin
+
+a, b, c, d, e, f = tuple(map(int, stdin.readline().split()))
+
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            print(x, y)
