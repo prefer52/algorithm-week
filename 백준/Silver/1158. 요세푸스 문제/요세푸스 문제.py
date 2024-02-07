2a05a5dfc8b57bc0ee0b90fef8e8a6a43433ae5c
prefer52 <110543937+prefer52@users.noreply.github.com>
2024-02-07 22:10:30 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,11 +1,11 @@
 n, k = map(int, input().split())
 
-result = ''
+result = []
 people = [str(i + 1) for i in range(n)]
 
 index = 0
 while people:
     index = (index + k - 1) % len(people)
-    result += people.pop(index) + ', '
-    
-print('<' + result[:-2] + '>')
+    result += [people.pop(index)]
+
+print('<' + ', '.join(result) + '>')
