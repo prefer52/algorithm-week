26ef6c576e5e2721f21fe7e8f00e0fb19eb17157
prefer52 <110543937+prefer52@users.noreply.github.com>
2024-02-18 15:46:33 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 46.07 ms, Memory: 26.5 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -1,23 +1,20 @@
 from collections import Counter, deque
 
 def solution(food_times, k):
-    # 시간이 작은 순으로 정렬
-    counter = deque(sorted((Counter(food_times).items())))
-    # 남은 음식 수, 음식마다 소요된 공통 시간
+    times = deque(sorted((Counter(food_times).items())))
     food_len, overtime = len(food_times), 0
-    while counter and k - (counter[0][0]-overtime)*food_len >= 0:
-        k -= (counter[0][0]-overtime)*food_len
-        food_len -= counter[0][1]
-        overtime = counter[0][0]
-        counter.popleft()
+    for time, foods_count in list(times):
+        remain_time = k - (time - overtime)*food_len
+        if not times:
+            return -1
+        if remain_time < 0:
+            break
+        k, overtime = remain_time, time
+        food_len -= foods_count
+        times.popleft()
 
-    if not counter:
+    if not times:
         return -1
-
+    
     k %= food_len
-    index = -1
-    for i, time in enumerate(food_times):
-        if time >= counter[0][0]:
-            index += 1
-            if index == k:
-                return i+1
+    return [i+1 for i in range(len(food_times)) if food_times[i] >= times[0][0]][k]
