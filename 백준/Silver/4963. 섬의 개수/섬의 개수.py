fe882f19b9b06646b028ca9a56242787720521b8
Seonho Lee <prefer0502@gmail.com>
2025-03-20 16:34:59 +0900

    [Silver II] Title: 섬의 개수, Time: 76 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,29 +1,32 @@
 from sys import stdin
 from collections import deque
 
-results, space = [], []
 MOVES = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+results = []
 while True:
     w, h = map(int, stdin.readline().split())
     if (w, h) == (0, 0):
         break
-    space = [[0]*(w+2)] + [[0] + list(map(int, stdin.readline().split())) + [0] for i in range(h)] + [[0]*(w+2)]
+    space = [[0] * (w+2)] + [[0] +list(map(int, stdin.readline().split())) + [0] for _ in range(h)] + [[0] * (w+2)]
+    
     result = 0
-
-    for x in range(1, w+1):
-        for y in range(1, h+1):
+    for x in range(1, w + 1):
+        for y in range(1, h + 1):
             if space[y][x] == 0:
                 continue
             result += 1
             deq = deque()
+            space[y][x] = 1
             deq.append((y, x))
             
             while deq:
-                ny, nx = deq.pop()
-                space[ny][nx] = 0
+                ny, nx = deq.popleft()
                 for dy, dx in MOVES:
-                    if space[ny + dy][nx + dx] == 1:
-                        deq.append((ny + dy, nx + dx))
+                    dy, dx = ny + dy, nx + dx
+                    if space[dy][dx] == 1:
+                        space[dy][dx] = 0
+                        deq.append((dy, dx))
+                        
     results.append(str(result))
     
 print('\n'.join(results))
