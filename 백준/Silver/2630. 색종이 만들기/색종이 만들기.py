262e775e777a88db493901d955e9b4bdfcdd3c17
Seonho Lee <prefer0502@gmail.com>
2025-07-15 20:41:10 +0900

    [Silver II] Title: 색종이 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+n = int(stdin.readline())
+color_paper = [list(map(int, stdin.readline().split())) for _ in range(n)]
+color_count = [0, 0]
+
+
+def count_paper(x1, y1, n):
+    if n == 1:
+        color_count[color_paper[y1][x1]] += 1
+        return
+    
+    init_color = color_paper[y1][x1]
+    for i in range(y1, y1+n):
+        for j in range(x1, x1+n):
+            if color_paper[i][j] != init_color:
+                break
+        else:
+            continue
+        break
+    else:
+        color_count[init_color] += 1
+        return
+    
+    count_paper(x1, y1, n//2)
+    count_paper(x1 + n//2, y1, n//2)
+    count_paper(x1, y1 + n // 2, n//2)
+    count_paper(x1 + n // 2, y1 + n // 2, n//2)
+
+count_paper(0, 0, n)
+print(color_count[0], color_count[1], sep="\n")
