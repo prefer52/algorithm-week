5ac1ae1a7c630c4c943cb07341512cb48498bba6
Seonho Lee <prefer0502@gmail.com>
2025-07-15 22:53:58 +0900

    [Silver II] Title: 색종이 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -4,7 +4,6 @@ n = int(stdin.readline())
 color_paper = [list(map(int, stdin.readline().split())) for _ in range(n)]
 color_count = [0, 0]
 
-
 def count_paper(x1, y1, n):
     if n == 1:
         color_count[color_paper[y1][x1]] += 1
@@ -14,18 +13,14 @@ def count_paper(x1, y1, n):
     for i in range(y1, y1+n):
         for j in range(x1, x1+n):
             if color_paper[i][j] != init_color:
-                break
-        else:
-            continue
-        break
-    else:
-        color_count[init_color] += 1
-        return
+                n //= 2
+                count_paper(x1, y1, n)
+                count_paper(x1 + n, y1, n)
+                count_paper(x1, y1 + n, n)
+                count_paper(x1 + n, y1 + n, n)
+                return
+    color_count[init_color] += 1
+    return
     
-    count_paper(x1, y1, n//2)
-    count_paper(x1 + n//2, y1, n//2)
-    count_paper(x1, y1 + n // 2, n//2)
-    count_paper(x1 + n // 2, y1 + n // 2, n//2)
-
 count_paper(0, 0, n)
 print(color_count[0], color_count[1], sep="\n")
