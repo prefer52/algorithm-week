0ca8d3b8fe17c182ad358ddf9c325f6d9cf71421
Seonho Lee <prefer0502@gmail.com>
2025-07-17 00:37:33 +0900

    [Silver I] Title: 포도주 시식, Time: 56 ms, Memory: 34456 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+n = int(stdin.readline())
+grapes = [int(stdin.readline()) for _ in range(n)] + [0]
+grape_sum = sum(grapes)
+
+dp = [[0, 0] for _ in range(n+1)]
+for i in range(n):
+    dp[i][0] = max(dp[i-1][1], dp[i-2][1] + grapes[i-1]) + grapes[i]
+    dp[i][1] = max(dp[i-1])
+        
+print(max(dp[n-1]))
