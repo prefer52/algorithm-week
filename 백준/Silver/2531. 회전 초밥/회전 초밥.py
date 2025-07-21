06d3d08d7acb6418b92765fa0ec432c4dea99776
Seonho Lee <prefer0502@gmail.com>
2025-07-21 14:49:04 +0900

    [Silver I] Title: 회전 초밥, Time: 68 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -0,0 +1,20 @@
+from sys import stdin
+
+n, d, k, c = map(int, stdin.readline().split())
+sushis = [c] + [int(stdin.readline()) for _ in range(n)]
+sushis += sushis[1:k]
+cur_sushis = [0]*(d+1)
+cur_sushi_set = set(sushis[:k+1])
+for i in range(k+1):
+    cur_sushis[sushis[i]] += 1
+max_result = len(cur_sushi_set)
+
+for i in range(k+1, len(sushis)):
+    cur_sushis[sushis[i-k]] -= 1
+    if cur_sushis[sushis[i-k]] == 0:
+        cur_sushi_set.remove(sushis[i-k])
+    cur_sushis[sushis[i]] += 1
+    cur_sushi_set.add(sushis[i])
+    max_result = max(len(cur_sushi_set), max_result)
+    
+print(max_result)
