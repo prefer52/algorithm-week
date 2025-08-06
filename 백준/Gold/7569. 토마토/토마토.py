0e48460b76d6ca49628150c35e5fdd2e08d99662
Seonho Lee <prefer0502@gmail.com>
2025-08-06 22:05:21 +0900

    [Gold V] Title: 토마토, Time: 2860 ms, Memory: 52252 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -2,17 +2,18 @@ from sys import stdin
 from collections import deque
 
 m, n, h = map(int, stdin.readline().split())
-tomatos_tower = []
+tomatos_tower = [[[-1]*(m+2) for _ in range(n+2)]]
 MOVES = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 deq = deque()
 
 for _ in range(h):
-    tomatos = [list(map(int, stdin.readline().split())) for _ in range(n)]
+    tomatos = [[-1]*(m+2)] + [[-1] + list(map(int, stdin.readline().split())) + [-1] for _ in range(n)] + [[-1]*(m+2)]
     tomatos_tower.append(tomatos)
+tomatos_tower.append([[-1]*(m+2) for _ in range(n+2)])
     
-for x in range(m):
-    for y in range(n):
-        for z in range(h):
+for x in range(1, m+1):
+    for y in range(1, n+1):
+        for z in range(1, h+1):
             if tomatos_tower[z][y][x] == 1:
                 deq.append((x, y, z, 0))
 
@@ -21,14 +22,14 @@ while deq:
     x, y, z, day = deq.popleft()
     for dx, dy, dz in MOVES:
         nx, ny, nz = x + dx, y + dy, z + dz
-        if 0 <= nx < m and 0 <= ny < n and 0 <= nz < h and tomatos_tower[nz][ny][nx] == 0:
+        if tomatos_tower[nz][ny][nx] == 0:
             tomatos_tower[nz][ny][nx] = 1
             deq.append((nx, ny, nz, day+1))
 
 result = day
-for x in range(m):
-    for y in range(n):
-        for z in range(h):
+for x in range(1, m+1):
+    for y in range(1, n+1):
+        for z in range(1, h+1):
             if tomatos_tower[z][y][x] == 0:
                 result = -1
                 break
